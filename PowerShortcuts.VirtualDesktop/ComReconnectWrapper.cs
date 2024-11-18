6a0b75cf97421e602054514ff37c787e19ccca9e
Petr Deriabin <peterder72@osetr.su>
2024-11-18 19:42:04 +0100

    Handling COM server restart

---
+++ PowerShortcuts.VirtualDesktop/ComReconnectWrapper.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.InteropServices;
+
+namespace PowerShortcuts.VirtualDesktop;
+
+internal sealed class ComReconnectWrapper<T>(Func<T> instanceFactory, Action onReconnect) where T : class
+{
+    private const int ComConnectionFailedCode = -2147023174;
+
+    private T m_Instance = instanceFactory.Invoke();
+
+    public void Execute(Action<T> action, bool retry = true)
+    {
+        try
+        {
+            action(m_Instance);
+        }
+        catch (COMException e) when (e.ErrorCode == ComConnectionFailedCode)
+        {
+            if (!retry) throw CreateAggregateException(e);
+            
+            OnReconnect();
+            Execute(action, false);
+        }
+    }
+
+    public TRet Execute<TRet>(Func<T, TRet> action, bool retry = true)
+    {
+        try
+        {
+            return action(m_Instance);
+        }
+        catch (COMException e) when (e.ErrorCode == ComConnectionFailedCode)
+        {
+            if (!retry) throw CreateAggregateException(e);
+
+            OnReconnect();
+            return Execute(action, false);
+        }
+    }
+
+    private void OnReconnect()
+    {
+        onReconnect.Invoke();
+        m_Instance = instanceFactory.Invoke();
+    }
+
+    private static AggregateException CreateAggregateException(COMException e) =>
+        new("Failed to reconnect to COM instance after multiple attempts", e);
+}
--- PowerShortcuts.VirtualDesktop/WindowManagerLogging.cs
+++ PowerShortcuts.VirtualDesktop/WindowManagerLogging.cs
@@ -6,4 +6,7 @@ internal static partial class WindowManagerLogging
 {
     [LoggerMessage(LogLevel.Warning, "Could not get window title, return code={returnCode}")]
     public static partial void LogUnsuccessfulGetWindowTitle(ILogger logger, int returnCode);
+    
+    [LoggerMessage(LogLevel.Warning, "Caught exception, possibly due to restarted explorer.exe. Reinitializing COM")]
+    public static partial void LogComException(ILogger logger);
 }
